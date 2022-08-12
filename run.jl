6b0d0f1a1e8538117036cffbb8e38e64373c260b
Taketo Sano <taketo1024@gmail.com>
2022-08-12 21:24:51 +0900

    update run.jl

--- run.jl
+++ run.jl
@@ -1,6 +1,6 @@
 using Khovanov.Links: Link
-using Khovanov.Jones: kauffmanBracket
+using Khovanov.Jones: jonesPolynomial
 
 l = Link([0, 0, 1, 1])
-f = kauffmanBracket(l)
+f = jonesPolynomial(l)
 println(f)
