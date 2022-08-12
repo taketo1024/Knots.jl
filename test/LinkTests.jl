24e927f47ddde2168ed0ba25265c83438d3f9504
Taketo Sano <taketo1024@gmail.com>
2022-08-12 21:16:15 +0900

    update LinkTests

--- test/LinkTests.jl
+++ test/LinkTests.jl
@@ -1,6 +1,6 @@
 module LinkTests
     using Test
-    using Khovanov.Links: Link, emptyLink, unknot, isEmpty, crossingNum, signedCrossingNums, writhe, components, mirror
+    using Khovanov.Links: Link, emptyLink, unknot, trefoil, figure8, hopfLink, isEmpty, crossingNum, signedCrossingNums, writhe, components, mirror
 
     function testAll()
         @testset "Links.jl" begin
@@ -50,35 +50,35 @@ module LinkTests
     end
 
     function testTrefoil()
-        k = Link([1,4,2,5],[3,6,4,1],[5,2,6,3])
+        k = trefoil
         @test length(components(k)) == 1
         @test crossingNum(k) == 3
         @test writhe(k) == -3
     end
 
     function testTrefoilMirror()
-        k = mirror(Link([1,4,2,5],[3,6,4,1],[5,2,6,3]))
+        k = mirror(trefoil)
         @test length(components(k)) == 1
         @test crossingNum(k) == 3
         @test writhe(k) == 3
     end
 
     function testFigureEight()
-        k = Link([4,2,5,1],[8,6,1,5],[6,3,7,4],[2,7,3,8])
+        k = figure8
         @test length(components(k)) == 1
         @test crossingNum(k) == 4
         @test writhe(k) == 0
     end
 
     function testFigureEightMirror()
-        k = mirror(Link([4,2,5,1],[8,6,1,5],[6,3,7,4],[2,7,3,8]))
+        k = mirror(figure8)
         @test length(components(k)) == 1
         @test crossingNum(k) == 4
         @test writhe(k) == 0
     end
 
     function testHopfLink() 
-        l = Link([4,1,3,2],[2,3,1,4])
+        l = hopfLink
         @test length(components(l)) == 2
         @test crossingNum(l) == 2
         @test writhe(l) == -2
