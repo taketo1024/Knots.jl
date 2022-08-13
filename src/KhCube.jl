aee908739855e031ae77a2560c09df1f561b1d1a
Taketo Sano <taketo1024@gmail.com>
2022-08-13 17:08:03 +0900

    add KhCube edge

--- src/KhCube.jl
+++ src/KhCube.jl
@@ -1,4 +1,4 @@
-# KhCube
+# KhCubeVertex
 
 struct KhCubeVertex
     state::State
@@ -6,7 +6,7 @@ struct KhCubeVertex
     generators::Vector{KhChainGenerator}
 end
 
-KhCubeVertex(l::Link, s::State) = begin
+function KhCubeVertex(l::Link, s::State) :: KhCubeVertex
     circles = components(resolve(l, s))
     r = length(circles)
     generators = map(0 : 2^r - 1) do i in
@@ -15,4 +15,61 @@ KhCubeVertex(l::Link, s::State) = begin
         KhChainGenerator(s, label)
     end
     KhCubeVertex(s, circles, generators)
+end
+
+# KhCube
+
+mutable struct KhCube{R} 
+    structure::KhAlgStructure{R}
+    link::Link
+    vertices::Dict{State, KhCubeVertex}
+end
+
+KhCube(str::KhAlgStructure{R}, l::Link) where {R} = KhCube(str, l, Dict{State, KhCubeVertex}())
+
+function dim(cube::KhCube{R}) where {R} 
+    crossingNum(cube.link)
+end
+
+function vertex(cube::KhCube{R}, u::State) :: KhCubeVertex where {R}
+    get!(cube.vertices, u, KhCubeVertex(cube.link, u))
+end
+
+abstract type EdgeType end
+
+struct mergeEdge <: EdgeType
+    from::Tuple{Int, Int}
+    to::Int
+end
+
+struct splitEdge <: EdgeType
+    from::Int
+    to::Tuple{Int, Int}
+end
+
+Base.findfirst(arr::AbstractArray{T, N}, elm::T) where {T, N} = findfirst( x -> x == elm, arr )
+
+function edge(cube::KhCube{R}, u::State, v::State) :: Union{mergeEdge, splitEdge} where {R}
+    @assert sum(u) + 1 == sum(v)
+    Cᵤ = vertex(cube, u).circles
+    Cᵥ = vertex(cube, v).circles
+    cᵤ = filter(c -> c ∉ Cᵥ, Cᵤ)
+    cᵥ = filter(c -> c ∉ Cᵤ, Cᵥ)
+
+    return if (length(cᵤ), length(cᵥ)) == (2, 1)
+        println(Cᵤ, Cᵥ, cᵤ, cᵥ)
+        i₁ = findfirst(Cᵤ, cᵤ[1])
+        i₂ = findfirst(Cᵤ, cᵤ[2])
+        j  = findfirst(Cᵥ, cᵥ[1])
+        mergeEdge((i₁, i₂), j)
+
+    elseif (length(cᵤ), length(cᵥ)) == (1, 2)
+        i  = findfirst(Cᵤ, cᵤ[1])
+        j₁ = findfirst(Cᵥ, cᵥ[1])
+        j₂ = findfirst(Cᵥ, cᵥ[2])
+        splitEdge(i, (j₁, j₂))
+
+    else
+        throw(Exception)
+    end
 end
--- test/KhCubeTests.jl
+++ test/KhCubeTests.jl
@@ -1,10 +1,12 @@
 using Test
 
 @testset "KhCube.jl" begin
-    using Khovanov: KhCubeVertex
+    using Khovanov: KhCube, KhCubeVertex, KhAlgGenerator, Kh, vertex, edge, mergeEdge, splitEdge
     using Khovanov: Link, unknot, trefoil
 
-    @testset "vertex" begin
+    R = Int
+
+    @testset "raw-vertex" begin
         l = Link([0, 0, 1, 1])
         v0 = KhCubeVertex(l, [0])
         v1 = KhCubeVertex(l, [1])
@@ -16,4 +18,48 @@ using Test
         @test length(v1.circles) == 1
         @test length(v1.generators) == 2
     end
+
+    @testset "vertex" begin
+        l = Link([0, 0, 1, 1])
+        A = KhAlgStructure{R}(Kh)
+        cube = KhCube(A, l)
+        v0 = vertex(cube, [0])
+        v1 = vertex(cube, [1])
+
+        @test v0.state == [0]
+        @test length(v0.circles) == 2
+        @test length(v0.generators) == 4
+        @test v1.state == [1]
+        @test length(v1.circles) == 1
+        @test length(v1.generators) == 2
+    end
+
+    @testset "edge-merge" begin
+        l = Link([0, 0, 1, 1])
+        A = KhAlgStructure{R}(Kh)
+        cube = KhCube(A, l)
+        e = edge(cube, [0], [1])
+
+        if isa(e, mergeEdge)
+            @test e.from == (1, 2)
+            @test e.to == 1
+        else
+            @test false
+        end
+    end
+
+    @testset "edge-split" begin
+        l = Link([0, 1, 1, 0])
+        A = KhAlgStructure{R}(Kh)
+        cube = KhCube(A, l)
+        e = edge(cube, [0], [1])
+
+        if isa(e, splitEdge)
+            @test e.from == 1
+            @test e.to == (1, 2)
+        else
+            @test false
+        end
+    end
+
 end
