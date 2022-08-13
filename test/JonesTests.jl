9e3277bac03bd553fb94c852064915ce3c272a44
Taketo Sano <taketo1024@gmail.com>
2022-08-13 09:29:03 +0900

    improve test structure.

--- test/JonesTests.jl
+++ test/JonesTests.jl
@@ -1,44 +1,40 @@
 using Test
-using Polynomials
-using Khovanov: Link, emptyLink, unknot, trefoil, figure8, hopfLink
-using Khovanov: jonesPolynomial, P
-
-q = variable(P)
-
-function testEmpty()
-    l = emptyLink
-    p = jonesPolynomial(l)
-    @test p == 1
-end
-
-function testUnknot()
-    l = unknot
-    p = jonesPolynomial(l)
-    @test p == q + q^(-1)
-end
-
-function testTrefoil()
-    l = trefoil
-    p = jonesPolynomial(l)
-    @test p == -q^(-9) + q^(-5) + q^(-3) + q^(-1)
-end
-
-function testFigure8()
-    l = figure8
-    p = jonesPolynomial(l)
-    @test p == q^5 + q^(-5)
-end
-
-function testHopfLink()
-    l = hopfLink
-    p = jonesPolynomial(l)
-    @test p == 1 + q^(-2) + q^(-4) + q^(-6)
-end
 
 @testset "Jones.jl" begin
-    testEmpty()
-    testUnknot()
-    testTrefoil()
-    testFigure8()
-    testHopfLink()
+    using Polynomials
+    using Khovanov: Link, emptyLink, unknot, trefoil, figure8, hopfLink
+    using Khovanov: jonesPolynomial, P
+
+    q = variable(P)
+
+    @testset "empty" begin
+        l = emptyLink
+        p = jonesPolynomial(l)
+        @test p == 1
+        x = 1
+    end
+
+    @testset "unknot" begin
+        l = unknot
+        p = jonesPolynomial(l)
+        @test p == q + q^(-1)
+    end
+
+    @testset "trefoil" begin
+        l = trefoil
+        p = jonesPolynomial(l)
+        @test p == -q^(-9) + q^(-5) + q^(-3) + q^(-1)
+    end
+
+    @testset "figure8" begin
+        l = figure8
+        p = jonesPolynomial(l)
+        @test p == q^5 + q^(-5)
+    end
+
+    @testset "hopfLink" begin
+        l = hopfLink
+        p = jonesPolynomial(l)
+        @test p == 1 + q^(-2) + q^(-4) + q^(-6)
+    end
 end
--- test/LinkTests.jl
+++ test/LinkTests.jl
@@ -1,89 +1,79 @@
 using Test
-using Khovanov: Link, isEmpty, crossingNum, signedCrossingNums, writhe, components, mirror, emptyLink, unknot, trefoil, figure8, hopfLink
-
-function testEmptyLink()
-    l = emptyLink
-    @test isEmpty(l)
-    @test crossingNum(l) == 0
-    @test writhe(l) == 0
-    @test length(components(l)) == 0
-end
-
-function testUnknot()
-    l = unknot
-    @test !isEmpty(l)
-    @test crossingNum(l) == 0
-    @test writhe(l) == 0
-    @test length(components(l)) == 1
-end
-
-function testOrientation() 
-    l1 = Link([0, 0, 1, 1])
-    @test signedCrossingNums(l1) == (1, 0)
-
-    l2 = Link([0, 1, 1, 0])
-    @test signedCrossingNums(l2) == (0, 1)
-end
-
-function testMirror()
-    l = Link([0, 0, 1, 1])
-    m = mirror(l)
-
-    @test signedCrossingNums(l) == (1, 0)
-    @test signedCrossingNums(m) == (0, 1)
-end
-
-function testTrefoil()
-    k = trefoil
-    @test length(components(k)) == 1
-    @test crossingNum(k) == 3
-    @test writhe(k) == -3
-end
-
-function testTrefoilMirror()
-    k = mirror(trefoil)
-    @test length(components(k)) == 1
-    @test crossingNum(k) == 3
-    @test writhe(k) == 3
-end
-
-function testFigureEight()
-    k = figure8
-    @test length(components(k)) == 1
-    @test crossingNum(k) == 4
-    @test writhe(k) == 0
-end
-
-function testFigureEightMirror()
-    k = mirror(figure8)
-    @test length(components(k)) == 1
-    @test crossingNum(k) == 4
-    @test writhe(k) == 0
-end
-
-function testHopfLink() 
-    l = hopfLink
-    @test length(components(l)) == 2
-    @test crossingNum(l) == 2
-    @test writhe(l) == -2
-end
-
-function test2CompUnlink()
-    l = Link([1,2,3,4], [3,2,1,4])
-    @test length(components(l)) == 2
-    @test crossingNum(l) == 2
-    @test writhe(l) == 0
-end
 
 @testset "Links.jl" begin
-    testEmptyLink()
-    testUnknot()
-    testOrientation()
-    testMirror()
-    testTrefoil()
-    testTrefoilMirror()
-    testFigureEight()
-    testFigureEightMirror()
-    testHopfLink()
-    test2CompUnlink()
+    using Khovanov: Link, isEmpty, crossingNum, signedCrossingNums, writhe, components, mirror, emptyLink, unknot, trefoil, figure8, hopfLink
+
+    @testset "emptyLink" begin
+        l = emptyLink
+        @test isEmpty(l)
+        @test crossingNum(l) == 0
+        @test writhe(l) == 0
+        @test length(components(l)) == 0
+    end
+
+    @testset "unknot" begin
+        l = unknot
+        @test !isEmpty(l)
+        @test crossingNum(l) == 0
+        @test writhe(l) == 0
+        @test length(components(l)) == 1
+    end
+
+    @testset "orientation" begin 
+        l1 = Link([0, 0, 1, 1])
+        @test signedCrossingNums(l1) == (1, 0)
+
+        l2 = Link([0, 1, 1, 0])
+        @test signedCrossingNums(l2) == (0, 1)
+    end
+
+    @testset "mirror" begin
+        l = Link([0, 0, 1, 1])
+        m = mirror(l)
+
+        @test signedCrossingNums(l) == (1, 0)
+        @test signedCrossingNums(m) == (0, 1)
+    end
+
+    @testset "trefoil" begin
+        k = trefoil
+        @test length(components(k)) == 1
+        @test crossingNum(k) == 3
+        @test writhe(k) == -3
+    end
+
+    @testset "trefoilMirror" begin
+        k = mirror(trefoil)
+        @test length(components(k)) == 1
+        @test crossingNum(k) == 3
+        @test writhe(k) == 3
+    end
+
+    @testset "figureEight" begin
+        k = figure8
+        @test length(components(k)) == 1
+        @test crossingNum(k) == 4
+        @test writhe(k) == 0
+    end
+
+    @testset "figureEightMirror" begin
+        k = mirror(figure8)
+        @test length(components(k)) == 1
+        @test crossingNum(k) == 4
+        @test writhe(k) == 0
+    end
+
+    @testset "hopfLink" begin 
+        l = hopfLink
+        @test length(components(l)) == 2
+        @test crossingNum(l) == 2
+        @test writhe(l) == -2
+    end
+
+    @testset "2compUnlink" begin 
+        l = Link([1,2,3,4], [3,2,1,4])
+        @test length(components(l)) == 2
+        @test crossingNum(l) == 2
+        @test writhe(l) == 0
+    end
 end
