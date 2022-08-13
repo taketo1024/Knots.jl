86e3b11df912e889ad5bb5566725997d89e30c90
Taketo Sano <taketo1024@gmail.com>
2022-08-13 14:55:42 +0900

    fix KhChain bug, add test.

--- src/KhChain.jl
+++ src/KhChain.jl
@@ -1,55 +1,72 @@
-using ComputationalHomology: AbstractChain
-
-import Base: length, copy, keys, values, getindex, setindex!, push!, iterate
+using ComputationalHomology
 
 # KhEnhancedState
 # Generators of Cube(D) and CKh(D)
 
-struct KhEnhancedState
+struct KhChainGenerator
     state::BitArray
     label::Vector{KhAlgGenerator}
 end
 
-asString(x::KhEnhancedState) = "$(join(x.label, ""))$(join(map(b -> b ? "₀" : "₁", x.state), ""))"
-Base.show(io::IO, x::KhEnhancedState) = print(io, asString(x))
+asString(x::KhChainGenerator) = "$(join(x.label, ""))$(join(map(b -> b ? "₀" : "₁", x.state), ""))"
+
+Base.show(io::IO, x::KhChainGenerator) = print(io, asString(x))
+Base.:(==)(x1::KhChainGenerator, x2::KhChainGenerator) = begin 
+    (x1.state, x1.label) == (x2.state, x2.label)
+end
+
 
 # KhChain
-# Elements of Cube(D) and CKh(D)
+# elements of Cube(D) and CKh(D)
 
-mutable struct KhChain{R} <: AbstractChain{KhEnhancedState,R}
+mutable struct KhChain{R} <: ComputationalHomology.AbstractChain{KhChainGenerator,R}
     dim::Int
-    elements::Dict{KhEnhancedState,R}
+    elements::Dict{KhChainGenerator,R}
 end
 
 KhChain(d::Int, elems::AbstractVector, coefs::AbstractVector) = KhChain(d, Dict(zip(elems, coefs)))
 KhChain(elems::AbstractVector, coefs::AbstractVector) = KhChain(0, elems, coefs)
 
-# implement interface
-dim(ch::KhChain{R}) where {R} = ch.dim
-length(ch::KhChain{R}) where {R} = length(ch.elements)
-copy(ch::KhChain{R}) where {R} = KhChain{R}(ch.dim, copy(ch.elements))
-keys(ch::KhChain{R}) where {R} = keys(ch.elements)
-values(ch::KhChain{R}) where {R} = values(ch.elements)
-getindex(ch::KhChain{R}, k::KhAlgGenerator) where {R} = get(ch.elements, k, zero(R))
-setindex!(ch::KhChain{R}, c::R, k::KhAlgGenerator) where {R} = setindex!(ch.elements, c, k)
+asString(ch::KhChain{R}) where {R} = begin
+    if isempty(ch.elements)
+        "0"
+    else
+        join( map( p -> "$(p[2])$(p[1])", collect(ch.elements) ), " + ")
+    end
+end
 
-function push!(ch::KhChain{R}, e::Pair{KhAlgGenerator,R}) where {R}
+# implement interface
+Base.zero(::Type{KhChain{R}}) where {R} = KhChain(0, Dict{KhChainGenerator, R}())
+Base.length(ch::KhChain{R}) where {R} = length(ch.elements)
+Base.copy(ch::KhChain{R}) where {R} = KhChain{R}(ch.dim, copy(ch.elements))
+Base.keys(ch::KhChain{R}) where {R} = keys(ch.elements)
+Base.values(ch::KhChain{R}) where {R} = values(ch.elements)
+Base.getindex(ch::KhChain{R}, k::KhChainGenerator) where {R} = get(ch.elements, k, zero(R))
+Base.setindex!(ch::KhChain{R}, c::R, k::KhChainGenerator) where {R} = setindex!(ch.elements, c, k)
+
+Base.push!(ch::KhChain{R}, e::Pair{KhChainGenerator, R}) where {R} = begin
     (k,v) = e
     if k ∈ ch
         ch.elements[k] += v
     else
         push!(ch.elements, e)
     end
-    ch
 end
 
-function iterate(ch::KhChain{R}, state...) where {R}
+Base.iterate(ch::KhChain{R}, state...) where {R} = begin
     y = iterate(ch.elements, state...)
     y === nothing && return nothing
     return (y[1], y[2])
 end
 
-function simplify(ch::KhChain)
+Base.show(io::IO, ch::KhChain{R}) where {R} = print(io, asString(ch))
+
+Base.:(==)(c1::KhChain{R}, c2::KhChain{R}) where {R} = begin 
+    (c1.dim, c1.elements) == (c2.dim, c2.elements)
+end
+
+ComputationalHomology.dim(ch::KhChain{R}) where {R} = ch.dim
+ComputationalHomology.simplify(ch::KhChain{R}) where {R} = begin
     for (k,v) in ch.elements
         v == zero(R) && delete!(ch.elements, k)
     end
--- test/KhChainTests.jl
+++ test/KhChainTests.jl
@@ -2,16 +2,83 @@ using Test
 
 @testset "KhChain.jl" begin
     using ComputationalHomology
-    using Khovanov: KhAlgStructure, Kh, X, I
-    using Khovanov: KhChain, dim, length
+    using Khovanov: KhChainGenerator, KhChain, Kh, X, I
 
     R = Int
     C = KhChain{R}
 
+    @testset "generator-equality" begin
+        x = KhChainGenerator([0], [X])
+        y = KhChainGenerator([0], [X])
+        z = KhChainGenerator([0], [I])
+        @test x == y
+        @test x ≠ z
+    end
+
     @testset "empty" begin
-        z = C(0, Dict())
-        @test dim(z) == 0
-        @test length(z) == 0
-        println(z)
+        c = zero(C)
+        @test dim(c) == 0
+        @test length(c) == 0
+    end
+
+    @testset "singleton" begin
+        x = KhChainGenerator([0], [X])
+        c = C(0, Dict(x => 1))
+        @test dim(c) == 0
+        @test length(c) == 1
+    end
+
+    @testset "index-access" begin
+        x = KhChainGenerator([0], [I])
+        c = zero(C)
+        @test c[x] == 0
+        c[x] = 1
+        @test c[x] == 1
+        c[x] = 0
+        @test c[x] == 0
+    end
+
+    @testset "add" begin
+        x = KhChainGenerator([0], [I])
+        y = KhChainGenerator([0], [X])
+        c = C(0, Dict(x => 1))
+        d = C(0, Dict(x => 3, y => 2))
+        e = c + d
+        @test length(e) == 2
+        @test e[x] == 4
+        @test e[y] == 2
+    end
+
+    @testset "scalar-mul" begin
+        x = KhChainGenerator([0], [I])
+        y = KhChainGenerator([0], [X])
+        c = C(0, Dict(x => 4, y => -3))
+        d = 2c
+        @test length(d) == 2
+        @test d[x] == 8
+        @test d[y] == -6
+    end
+    
+    @testset "equal" begin
+        x = KhChainGenerator([0], [I])
+        y = KhChainGenerator([0], [X])
+        c = C(0, Dict(x => 3, y => 2))
+        d = C(0, Dict(x => 3, y => 2))
+        e = C(0, Dict(x => 3, y => 1))
+        @test c == d
+        @test c ≠ e
+    end
+    
+    @testset "simplify" begin
+        x = KhChainGenerator([0], [I])
+        c = C(0, Dict(x => 0))
+
+        @test length(c) == 1
+        @test c ≠ zero(C)
+        
+        simplify(c)
+        
+        @test length(c) == 0
+        @test c == zero(C)
     end
 end
