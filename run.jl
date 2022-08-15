e739d3b1e99c799aa6154387e126459cf5802524
Taketo Sano <taketo1024@gmail.com>
2022-08-15 14:44:03 +0900

    update run.jl

--- run.jl
+++ run.jl
@@ -1,6 +1,24 @@
-using Khovanov: Link
-using Khovanov: jonesPolynomial
+using Khovanov: KhAlgStructure, KhHomology
+using Khovanov: Link, unknot, trefoil, figure8, mirror
 
-l = Link([0, 0, 1, 1])
-f = jonesPolynomial(l)
-println(f)
+types = [
+    "Z-Kh", 
+    "Q-Kh", 
+    "F2-Kh", 
+    "F3-Kh", 
+    "Q[h]-bigraded", 
+    "F2[h]-bigraded", 
+    "F3[h]-bigraded", 
+    "Q[t]-bigraded", 
+    "F2[t]-bigraded", 
+    "F3[t]-bigraded"
+]
+
+for t in types
+    l = trefoil
+    A = KhAlgStructure(t)
+    H = KhHomology(A, l)
+    println(H, "\n")
+end
+
+nothing
