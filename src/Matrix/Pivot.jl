285725513ad389c9c26f711d100c4fa1804cb4bb
Taketo Sano <taketo1024@gmail.com>
2022-08-18 01:30:55 +0900

    add findFLColumnPivots!

--- src/Matrix/Pivot.jl
+++ src/Matrix/Pivot.jl
@@ -12,28 +12,28 @@ using SparseArrays
 
 mutable struct Pivot{R<:RingElement}
     size::Tuple{Int, Int}
-    nonzero::Vector{Vector{Int}}   # row -> [col]
+    entries::Vector{Vector{Int}}   # row -> [col]
     rowHead::Vector{Int}           # row -> col
     rowWeight::Vector{Int}         # row -> weight
     colWeight::Vector{Int}         # col -> weight
     candidates::Vector{Set{Int}}   # row -> Set(cols)
-    hasPivot::Set{Int}             # Set(rows)
-    pivots::Vector{Int}            # col -> row
+    pivotRows::Set{Int}            # Set(rows)
+    pivotPos::Vector{Int}          # col -> row
 end
 
 Pivot(A::AbstractSparseMatrix{R}) where {R} = begin
     (m, n) = size(A)
-    nonzero = map( _ -> Int[], 1:m)
+    entries = map( _ -> Int[], 1:m)
     rowHead   = fill(0, m)
     rowWeight = fill(0, m)
     colWeight = fill(0, n)
     candidates = map( _ -> Set{Int}(), 1:m)
-    hasPivot = Set{Int}()
-    pivots = fill(0, n)
+    pivotRows = Set{Int}()
+    pivotPos = fill(0, n)
 
     for (i, j, a) in zip(findnz(A)...)
         iszero(a) && continue
-        push!(nonzero[i], j)
+        push!(entries[i], j)
         rowWeight[i] += 1
         colWeight[j] += 1
 
@@ -45,14 +45,14 @@ Pivot(A::AbstractSparseMatrix{R}) where {R} = begin
         end
     end
 
-    Pivot{R}((m, n), nonzero, rowHead, rowWeight, colWeight, candidates, hasPivot, pivots)
+    Pivot{R}((m, n), entries, rowHead, rowWeight, colWeight, candidates, pivotRows, pivotPos)
 end
 
-function findPivots(A::AbstractSparseMatrix{R}) where {R<:RingElement} 
+function findPivots!(A::AbstractSparseMatrix{R}) where {R<:RingElement} 
     @debug "find pivot: A = size$(size(A))"
     findFLPivots!(A)
-    # findFLColumnPivots!(A)
-    # findCycleFreePivots!(A)
+    # findFLColumnpivotPos!(A)
+    # findCycleFreepivotPos!(A)
 end
 
 function result(piv::Pivot) :: Vector{CartesianIndex{2}}
@@ -62,29 +62,62 @@ end
 # Faugère-Lachartre pivot search
 function findFLPivots!(piv::Pivot)
     m = piv.size[1]
-    tmp = Dict()
+    pivots = Dict()
+
     for i in 1:m
         j = piv.rowHead[i]
-        j == 0 && continue
-        if !haskey(tmp, j) || piv.rowWeight[i] < piv.rowWeight[tmp[j]]
-            tmp[j] = i
+        j ∉ piv.candidates[i] && continue
+
+        if !haskey(pivots, j) || piv.rowWeight[i] < piv.rowWeight[pivots[j]]
+            pivots[j] = i
         end
     end
 
-    for (j, i) in tmp
+    for (j, i) in pivots
         setPivot!(piv, i, j)
     end
 
-    @debug "FL-pivots: $(length(tmp))"
+    @debug "FL-pivots: $(length(piv.pivotRows))"
 end
 
 function findFLColumnPivots!(piv::Pivot)
+    before = length(piv.pivotRows)
+
+    m = piv.size[1]
+    occupied = reduce(piv.pivotRows; init=Set()) do res, i
+        union!(res, piv.entries[i])
+    end
+
+    rows = collect( setdiff(Set(1:m), piv.pivotRows) )
+    sort!(rows, by=(i -> piv.rowWeight[i]))
+
+    for i in rows
+        isempty(piv.entries[i]) && continue # skip empty rows
+        
+        candidates = Int[]
+
+        for j in piv.entries[i]
+            if j ∉ occupied && j ∈ piv.candidates[i] 
+                push!(candidates, j)
+            end
+        end
+
+        isempty(candidates) && continue
+
+        sort!(candidates, by=(j -> colWeight[j]))
+        j = first(candidates)
+
+        setPivot!(piv, i, j)
+        union!(occupied, piv.entries[i])
+    end
+
+    @debug "FL-col-pivots: $(length(piv.pivotRows) - before)"
 end
 
 function findCycleFreePivots!(piv::Pivot)
 end
 
 function setPivot!(piv::Pivot, i::Int, j::Int)
-    push!(piv.hasPivot, i)
-    piv.pivots[j] = i
+    push!(piv.pivotRows, i)
+    piv.pivotPos[j] = i
 end
--- test/Matrix/PivotTests.jl
+++ test/Matrix/PivotTests.jl
@@ -16,13 +16,13 @@ using Test
 
         piv = Pivot(A)
 
-        @test piv.nonzero == [[1,3,6,7,9], [2,3,4,6,8], [3,4,8,9], [2,3,5], [2,4,7,9],[1,3,5,6,8,9]]
+        @test piv.entries == [[1,3,6,7,9], [2,3,4,6,8], [3,4,8,9], [2,3,5], [2,4,7,9],[1,3,5,6,8,9]]
         @test piv.rowHead == [1,2,3,2,2,1]
         @test piv.rowWeight == [5,5,4,3,4,6]
         @test piv.colWeight == [2,3,5,3,2,3,2,3,4]
         @test piv.candidates == [Set([1,3,6,7,9]), Set([2,3,4,6]), Set([3,4,8,9]), Set([2,3]), Set([2,4,7,9]), Set([1,3,5,6,8,9])]
-        @test isempty(piv.hasPivot)
-        @test piv.pivots == fill(0, 9)
+        @test isempty(piv.pivotRows)
+        @test piv.pivotPos == fill(0, 9)
     end
 
     @testset "findFLPivots" begin
@@ -38,8 +38,28 @@ using Test
 
         findFLPivots!(piv)
 
-        @test piv.hasPivot == Set([1,4,5,6])
-        @test piv.pivots == [1,4,5,0,0,6,0,0,0]
+        @test piv.pivotRows == Set([1,4,5,6])
+        @test piv.pivotPos == [1,4,5,0,0,6,0,0,0]
+    end
+
+    @testset "findFLColumnPivots" begin
+        A = sparse([
+            1 0 0 0 0 1 0 0 1;
+            0 1 1 1 0 1 0 1 0;
+            0 0 1 1 0 0 0 1 1;
+            0 1 0 0 1 0 0 0 0;
+            0 0 1 0 0 0 0 0 0;
+            0 1 0 0 0 1 0 1 0
+        ])
+        piv = Pivot(A)
+
+        findFLPivots!(piv)
+        @test piv.pivotRows == Set([1,4,5])
+
+        findFLColumnPivots!(piv)
+
+        @test piv.pivotRows == Set([1,4,5,6,3])
+        @test piv.pivotPos == [1,4,5,3,0,0,0,6,0]
     end
 end
 
