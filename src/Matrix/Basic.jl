0869711d4b78bfb6695453d8ac7132c93f261459
Taketo Sano <taketo1024@gmail.com>
2022-08-19 18:52:06 +0900

    add workaround for print_array crash.

--- src/Matrix/Basic.jl
+++ src/Matrix/Basic.jl
@@ -38,3 +38,26 @@ function _toSparse(Aᵈ::DenseMatrix{R}) :: SparseMatrix{R} where {R<:RingElemen
 
     SparseArrays.sparse(Is, Js, Vs, m, n)
 end
+
+# MEMO: 
+# We need the following overrides to avoid crash when printing `A`, 
+# caused internally by calling `zero(R)` which doesn't exist for `RingElem`.
+# (still `@show`` on an empty matrix causes crash...)
+
+function Base.print_array(io::IO, A::SparseArrays.AbstractSparseMatrixCSC{R}) where {R<:RingElem} 
+    _, _, V = findnz(A)
+    if isempty(V)
+        B = spzeros(size(A))
+    else
+        v = first(V)
+        baseRing = parent(v)
+        B = Base.Matrix(_toDense(A, baseRing))
+    end
+    Base.print_array(io, B)
+end
+
+function Base.show(io::IO, A::SparseArrays.AbstractSparseMatrixCSC{R}) where {R<:RingElem} 
+    I, J, K = findnz(A)
+    m, n = size(A)
+    print(io, "sparse(", I, ", ", J, ", ", K, ", ", m, ", ", n, ")")
+end
