36d8252a0d4e5701caecf939f9d7f76679ba8930
Taketo Sano <taketo1024@gmail.com>
2022-08-24 16:31:31 +0900

    add GF workaround.

--- src/Extensions/GaloisFields.jl
+++ src/Extensions/GaloisFields.jl
@@ -10,6 +10,11 @@ function normalizing_unit(a::T) :: Tuple{T, T} where {T<:AbstractGaloisField}
     iszero(a) ? (one(T), one(T)) : (inv(a), a)
 end
 
+# workaround for Polynomial divrem
+function Base.isapprox(a::T, b::Int) :: Bool where {T<:AbstractGaloisField}
+    a == T(b)
+end
+
 function Base.gcdx(a::T, b::T) :: Tuple{T, T, T} where {T<:AbstractGaloisField}
     if iszero(a) 
         iszero(b) ? (zero(T), one(T), zero(T)) : (one(T), zero(T), inv(b))
