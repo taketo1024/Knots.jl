a22c8c2d9381b5b1ef2c40861f269541566bcd7b
Taketo Sano <taketo1024@gmail.com>
2022-08-25 10:40:53 +0900

    update run.jl

--- run.jl
+++ run.jl
@@ -3,48 +3,30 @@ using Knots.Links
 using Knots.Khovanov
 using Knots.Homology: compute_single, compute_incremental, compute_reverse_incremental
 
-function run1(H, msg="")
-    isempty(msg) || println(msg)
-    # for i in hDegRange(H)
-    i = 0
-        Hi = compute_single(H, i)
-        println("H[$i] = ", Hi)
-    # end
-end
-
-function run2(H, msg="")
-    isempty(msg) || println(msg)
-    prev = nothing
-    # for i in hDegRange(H)
-    i = 0
-        Hi, prev = compute_incremental(H, i; previous=prev)
-        println("H[$i] = ", Hi)
-    # end
-end
-
-function run3(H, msg="")
-    isempty(msg) || println(msg)
+function run(name::String, str::String="Kh"; mode::Int=1, only_zero::Bool=false)
+    println("run: $name, mode: $mode")
+    l = Link(name)
+    A = KhAlgStructure(str)
     H = KhHomology(A, l)
     prev = nothing
-    # for i in reverse(hDegRange(H))
-    i = 0
-        Hi, prev = compute_reverse_incremental(H, i; previous=prev)
+    range = only_zero ? 
+        (0:0) : 
+        (mode in [1, 2]) ? hDegRange(H) : reverse(hDegRange(H))
+
+    for i in range
+        if mode == 1
+            Hi = compute_single(H, i)
+        elseif mode == 2
+            Hi, prev = compute_incremental(H, i; previous=prev)
+        elseif mode == 3
+            Hi, prev = compute_reverse_incremental(H, i; previous=prev)
+        end
         println("H[$i] = ", Hi)
-    # end
+    end
 end
 
 @show Threads.nthreads()
-
-A = KhAlgStructure("Z-Kh")
-
-@time run1(KhHomology(A, unknot), "test-run")
-
-l = Link([4,2,5,1],[8,4,9,3],[5,12,6,13],[2,8,3,7],[9,19,10,18],[11,6,12,7],[13,22,14,1],[15,20,16,21],[17,11,18,10],[19,16,20,17],[21,14,22,15])
-H = KhHomology(A, l)
-
-@profview @time run1(H, "compute-single")
-@profview @time run2(H, "compute-incremental")
-@profview @time run3(H, "compute-reverse-incremental")
+@time run("3_1")
 
 nothing
 
