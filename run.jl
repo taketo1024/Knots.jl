fd8cbd85aa6344da6f9aca7849469ecd7f0ab278
Taketo Sano <taketo1024@gmail.com>
2022-09-09 23:47:56 -0700

    add packages, modify run.jl

--- run.jl
+++ run.jl
@@ -1,12 +1,46 @@
 using Knots.Links, Knots.Khovanov
+using Knots.Extensions: GaussInt
+using PyCall
+using CSV
+using DataFrames
 
 ENV["JULIA_DEBUG"] = "Knots"
 
-L = trefoil
-c = 2
-r = true
+sg = pyimport("spherogram")
 
-@time s = s_c(L, c; reduced=r)
-@show s
+function run(name::String, file="result.csv") :: Bool
+    pdcode = try
+        sg.Link(name).PD_code()
+    catch
+        return false
+    end
+
+    L = Link(pdcode)
+
+    @time s_2  = s_c(L, 2)
+    @time s_3  = s_c(L, 3)
+    @time s_1i = s_c(L, GaussInt(1, 1))
+    
+    @show name s_2 s_3 s_1i
+
+    result = DataFrame(
+        name = name, 
+        s_2 = s_2, 
+        s_3 = s_3, 
+        s_1i = s_1i
+    )
+
+    CSV.write(file, result; append = isfile(file))
+    true
+end
+
+for i in 11:14
+    for j in 1:10000
+        name = "K$(i)n$(j)"
+        next = run(name)
+        !next && break 
+    end
+end
+
 
 nothing
